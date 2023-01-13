95eac906cf09ac8450f7630e0b29377810f92cc3
Jake Fenley <jfenley6@gmail.com>
2023-01-13 23:15:28 +0000

    add husky

--- src/validation-middleware.ts
+++ src/validation-middleware.ts
@@ -87,7 +87,8 @@ export const validationMiddleware = <H, P, Q, B, R>(
       if (opts?.exposeResponseErrors) {
         ctx.status = 500;
         ctx.type = 'json';
-        ctx.body = { output }; ctx.app.emit('error', new ValidationError({ output }), ctx);
+        ctx.body = { output };
+        ctx.app.emit('error', new ValidationError({ output }), ctx);
         return;
       }
 
