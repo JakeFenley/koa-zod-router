d4bc239f5077978cc3f73f8f6a55714ecb9cff69
Jake Fenley <jfenley6@gmail.com>
2023-01-14 03:39:53 +0000

    util tests

--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,5 @@
 import { Context, DefaultState, Middleware, Next } from 'koa';
-import { RegisterSpec, Spec, ValidationOptions, ZodContext, ZodMiddleware } from './types';
+import { ValidationOptions, ZodContext, ZodMiddleware } from './types';
 
 function flatten<H, P, Q, B, R>(
   middlewares: Array<ZodMiddleware<H, P, Q, B, R> | undefined>,
@@ -27,60 +27,6 @@ export const prepareMiddleware = <H, P, Q, B, R>(
 export async function noopMiddleware(ctx: Context, next: Next) {
   return await next();
 }
-
-export const assertString = (val: any): val is string => {
-  return typeof val === 'string';
-};
-
-export const assertRegex = (val: any): val is RegExp => {
-  return val instanceof RegExp;
-};
-
-export const assertStringOrRegex = (val: any): val is string | RegExp => {
-  if (assertString(val) || assertRegex(val)) {
-    return true;
-  }
-
-  return false;
-};
-
-export const assertMiddlewares = (val: any): val is Middleware | Middleware[] => {
-  if (typeof val === 'function') {
-    return true;
-  }
-
-  if (Array.isArray(val) && typeof val[0] === 'function') {
-    return true;
-  }
-
-  return false;
-};
-
-export const assertPath = (val: any): val is string | RegExp | Array<string | RegExp> => {
-  if (assertString(val)) {
-    return true;
-  }
-
-  if (Array.isArray(val)) {
-    if (val[0] instanceof RegExp) {
-      return true;
-    }
-
-    if (typeof val[0] === 'string') {
-      return true;
-    }
-  }
-
-  return false;
-};
-
-export const assertSpec = <H, P, Q, B, R>(val: any): val is Spec<H, P, Q, B, R> | RegisterSpec<H, P, Q, B, R> => {
-  if (typeof val === 'object' && val['path']) {
-    return true;
-  }
-
-  return false;
-};
 
 export const assertValidation = <H, P, Q, B, R>(val: any): val is ValidationOptions<H, P, Q, B, R> => {
   const props = ['headers', 'body', 'query', 'params', 'response'];
--- test/util.spec.ts
+++ test/util.spec.ts
@@ -1,9 +1,82 @@
 import assert from 'assert';
 import { describe, it } from 'node:test';
-import { prepareMiddleware } from '../src/util';
+import { prepareMiddleware, assertValidation } from '../src/util';
 
 describe('prepareMiddleware', () => {
   it('should flatten array of handlers', () => {
-    const middlewares = prepareMiddleware([() => {}, [() => {}, () => {}]]);
+    let execOrder: number[] = [];
+
+    const middlewares = prepareMiddleware([
+      () => {
+        execOrder.push(0);
+      },
+      [
+        () => {
+          execOrder.push(1);
+        },
+        () => {
+          execOrder.push(2);
+        },
+      ],
+      [
+        () => {
+          execOrder.push(3);
+        },
+        () => {
+          execOrder.push(4);
+        },
+        () => {
+          execOrder.push(5);
+        },
+      ],
+      () => {
+        execOrder.push(6);
+      },
+    ]);
+
+    for (const fn of middlewares) {
+      // @ts-ignore
+      fn({}, {});
+    }
+
+    assert.deepEqual(execOrder, [0, 1, 2, 3, 4, 5, 6]);
+  });
+});
+
+describe('assertValidation', () => {
+  it('should return false when validation props are missing', () => {
+    const validation = assertValidation({});
+
+    assert.equal(validation, false);
+  });
+
+  it('should return true when validation prop params exists', () => {
+    const validation = assertValidation({ params: {} });
+    assert.equal(validation, true);
+  });
+
+  it('should return true when validation prop headers exists', () => {
+    const validation = assertValidation({ headers: {} });
+    assert.equal(validation, true);
+  });
+
+  it('should return true when validation prop query exists', () => {
+    const validation = assertValidation({ query: {} });
+    assert.equal(validation, true);
+  });
+
+  it('should return true when validation prop body exists', () => {
+    const validation = assertValidation({ body: {} });
+    assert.equal(validation, true);
+  });
+
+  it('should return true when validation prop response exists', () => {
+    const validation = assertValidation({ response: {} });
+    assert.equal(validation, true);
+  });
+
+  it('should return true when provided a mix of props', () => {
+    const validation = assertValidation({ response: {}, params: {}, body: {} });
+    assert.equal(validation, true);
   });
 });
